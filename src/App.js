05eda8f38b909e31361201b8473306f5a7b06f37
Ali-consciousroad <ali_dindar@live.be>
2021-03-15 15:58:58 +0100

    Section 2 react client and node server setup : 2.1 React app setup -> 2.5 Node server

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+// Arrow function: Parenthesis will word only with one statement otherwise we need to use brackets.
+const App = () => (
+  <div>
+    <h1>MERN CRUD</h1>
+  </div>
+);
+
+export default App;
