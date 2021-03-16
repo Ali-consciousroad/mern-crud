f438acb39b22ea105c2e1b0cbe235349ec4111b8
Ali-consciousroad <ali_dindar@live.be>
2021-03-16 17:24:53 +0100

    connect the db to mongo atlas cluster + issues causing the server.js to crash solved

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 // Arrow function: Parenthesis will word only with one statement otherwise we need to use brackets.
 const App = () => (
   <div>
-    <h1>MERN CRUD</h1>
+    <h1>MERN CRUD APP</h1>
   </div>
 );
 
--- server/server.js
+++ server/server.js
@@ -3,11 +3,24 @@ const morgan = require('morgan')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const mongoose = require('mongoose')
+// Make it possible to access the .env file
 require('dotenv').config()
-
 
 // app
 const app = express()
+
+// db config for mongoDBAtlas
+// Config we will always have to use with mongoose
+mongoose
+  .connect(process.env.DATABASE, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true
+  })
+  .then(() => console.log('DB connected'))
+  // Log the error if any
+  .catch(err => console.log(err));
 
 // middlewares
 app.use(cors())
@@ -17,7 +30,7 @@ app.use(bodyParser.json())
 // route
 app.get('*', (req, res) => {
   res.json({
-    data: 'You reached nodejs api for react node crud app'
+    data: 'You reached nodejs api for react node crud app test'
   })
 })
 
