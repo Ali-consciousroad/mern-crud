3a4edbbe31b1dc29f9b9f8fa9ee1b2e6e2a24d74
Ali-consciousroad <ali_dindar@live.be>
2021-03-19 15:17:13 +0100

    Section4.11 - Using postman: create post request to test the api with postman by using json

--- server/controllers/post.js
+++ server/controllers/post.js
@@ -1,6 +1,13 @@
+const Post = require('../models/post');
+
 // Take out the logic (the arrow function) from the routes
 exports.create = (req, res) => {
+  /*
   res.json({
     data: 'Hello from the controller'
   });
+  */
+    // Just show us what we'll get inside the console
+    console.log(req.body);
+
 };
--- server/routes/post.js
+++ server/routes/post.js
@@ -7,7 +7,9 @@ const router = express.Router();
 // Import the logic from the controller methods
 const { create } = require('../controllers/post');
 // app.get() changed by router.get()
-router.get('/post', create);
+// Will be handled by the controller method
+// Changed from router.get -> router.post to test the api post request with Postman
+router.post('/post', create);
 
 //
 module.exports = router;
