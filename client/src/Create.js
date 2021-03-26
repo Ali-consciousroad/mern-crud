7ae21ee30306d2c3debcd8aa9a7b909faa73c52c
Ali-consciousroad <ali_dindar@live.be>
2021-03-26 16:49:42 +0100

    Section5 - 4.17 Axios post request: Send our post request to our DB (Mongo Atlas)

--- client/src/Create.js
+++ client/src/Create.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import axios from 'axios'
 // Arrow function: Parenthesis will word only with one statement otherwise we need to use brackets.
 const Create = () => {
 
@@ -20,7 +21,7 @@ const Create = () => {
     // Used to check output inside the console of the browser
     // console.log('name', name, 'event', event);
     setState({...state, [name]: event.target.value});
-  }
+  };
 
   // Old school function
   /*
@@ -31,19 +32,36 @@ const Create = () => {
   }
   */
 
+  const handleSubmit = event => {
+    event.preventDefault();
+    /*console.table({title, content, user});*/
+    axios
+      .post(`${process.env.REACT_APP_API}/post`, { title, content, user})
+      .then(response => {
+        console.log(response);
+        //empty state
+        setState({...state, title: '', content: '', user: ''});
+        //show success alert
+        alert(`Post titled ${response.data.title} is created`);
+      })
+      .catch(error => {
+        console.log(error.response);
+        alert(error.response.data.error);
+      });
+  };
 
 
   return (
     <div className="container p-5">
       <h1>CREATE POST</h1>
-      // Secon way to show state: Display state information directly inside our /create page
-      {JSON.stringify(state)}
-      <form>
+      {/* Second way to show state: Display state information directly inside our /create page */}
+      {/* {JSON.stringify(state)} */}
+      <form onSubmit={handleSubmit}>
         <div className="form-group">
           <label className="text-muted">Title</label>
-          /*<input value={state.title} type="text" className="form-control" placeholder="Post title" required /> --
+          {/*<input value={state.title} type="text" className="form-control" placeholder="Post title" required /> --
           Shorter syntax allowed thanks to state destructuration
-          Add event handler to handle any change made on the component */
+          Add event handler to handle any change made on the component */}
           <input
             onChange={handleChange('title')}
             value={title}
@@ -55,14 +73,14 @@ const Create = () => {
 
         <div className="form-group">
           <label className="text-muted">Content</label>
-          // <input value={state.content} type="text" className="form-control" placeholder="Write something" required />
+          {/* <input value={state.content} type="text" className="form-control" placeholder="Write something" required /> */}
           <textarea onChange={handleChange('content')} value={content} type="text" className="form-control" placeholder="Post title" required />
 
         </div>
 
         <div className="form-group">
           <label className="text-muted">User</label>
-          // <input value={state.user} type="text" className="form-control" placeholder="Your name" required />
+          {/* <input value={state.user} type="text" className="form-control" placeholder="Your name" required /> */}
           <input onChange={handleChange('user')} value={user} type="text" className="form-control" placeholder="Post title" required />
 
         </div>
