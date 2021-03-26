9d474b6e4180c943181a7535e7bd7d2fe7ecd8e3
Ali-consciousroad <ali_dindar@live.be>
2021-03-26 15:27:39 +0100

    Section5: Create Post Client - 16. On change handler: Add the on change handler for our Create.js page

--- client/src/Create.js
+++ client/src/Create.js
@@ -1,31 +1,78 @@
-import React from 'react';
+import React, {useState} from 'react';
 // Arrow function: Parenthesis will word only with one statement otherwise we need to use brackets.
-const Create = () => (
-  <div className="container p-5">
-    <h1>CREATE POST</h1>
-    <form>
+const Create = () => {
 
-      <div className="form-group">
-        <label className="text-muted">Title</label>
-        <input type="text" className="form-control" placeholder="Post title" required />
-      </div>
+  // This will allow react to control all the HTML components
+  // state
+  const [state, setState] = useState({
+      title: '',
+      content: '',
+      user: ''
+  });
 
-      <div className="form-group">
-        <label className="text-muted">Content</label>
-        <input type="text" className="form-control" placeholder="Write something" required />
-      </div>
+  // destructure values from state
+  const {title, content, user} = state;
 
-      <div className="form-group">
-        <label className="text-muted">User</label>
-        <input type="text" className="form-control" placeholder="Your name" required />
-      </div>
+  // First way : Arrow function
+  // Create the event handler function
+  // We add a function (an anonymous function) inside another function (the handleChange function)
+  const handleChange = (name) => (event) => {
+    // Used to check output inside the console of the browser
+    // console.log('name', name, 'event', event);
+    setState({...state, [name]: event.target.value});
+  }
 
-      <div>
-        <button className="btn btn-primary">Create</button>
-      </div>
+  // Old school function
+  /*
+  function handleChange(name) {
+    return function(event){
+      setState({...state, [name]: event.target.value });
+    };
+  }
+  */
 
-    </form>
-  </div>
-);
+
+
+  return (
+    <div className="container p-5">
+      <h1>CREATE POST</h1>
+      // Secon way to show state: Display state information directly inside our /create page
+      {JSON.stringify(state)}
+      <form>
+        <div className="form-group">
+          <label className="text-muted">Title</label>
+          /*<input value={state.title} type="text" className="form-control" placeholder="Post title" required /> --
+          Shorter syntax allowed thanks to state destructuration
+          Add event handler to handle any change made on the component */
+          <input
+            onChange={handleChange('title')}
+            value={title}
+            type="text"
+            className="form-control"
+            placeholder="Post title"
+            required />
+        </div>
+
+        <div className="form-group">
+          <label className="text-muted">Content</label>
+          // <input value={state.content} type="text" className="form-control" placeholder="Write something" required />
+          <textarea onChange={handleChange('content')} value={content} type="text" className="form-control" placeholder="Post title" required />
+
+        </div>
+
+        <div className="form-group">
+          <label className="text-muted">User</label>
+          // <input value={state.user} type="text" className="form-control" placeholder="Your name" required />
+          <input onChange={handleChange('user')} value={user} type="text" className="form-control" placeholder="Post title" required />
+
+        </div>
+
+        <div>
+          <button className="btn btn-primary">Create</button>
+        </div>
+      </form>
+    </div>
+  )
+}
 
 export default Create;
