456faef84506069e8c57be485b360e13c74b5c1e
Ali-consciousroad <ali_dindar@live.be>
2021-10-20 12:01:46 +0200

    5.16 Use state

--- client/src/Create.js
+++ client/src/Create.js
@@ -1,23 +1,43 @@
-import React from 'react';
+import React, {useState} from 'react';
 // Arrow function: Parenthesis will word only with one statement otherwise we need to use brackets.
-const Create = () => (
+const Create = () => {
+
+  // State
+  const [state, setState] = useState({
+    title: '',
+    content: '', 
+    user: ''
+  })
+
+    // We'll grab the state content like this 
+  /**
+   state.title
+   state.content
+   */
+
+  // destructure values from state
+  // So we don't need to write state.title anymore
+  // We can just write the destructured value  
+  const {title, content, user} = state
+
+  return (
   <div className="container p-5">
     <h1>CREATE POST</h1>
     <form>
 
       <div className="form-group">
         <label className="text-muted">Title</label>
-        <input type="text" className="form-control" placeholder="Post title" required />
+        <input value={title } type="text" className="form-control" placeholder="Post title" required />
       </div>
 
       <div className="form-group">
         <label className="text-muted">Content</label>
-        <input type="text" className="form-control" placeholder="Write something" required />
+        <input value={content} type="text" className="form-control" placeholder="Write something" required />
       </div>
 
       <div className="form-group">
         <label className="text-muted">User</label>
-        <input type="text" className="form-control" placeholder="Your name" required />
+        <input value={user} type="text" className="form-control" placeholder="Your name" required />
       </div>
 
       <div>
@@ -26,6 +46,6 @@ const Create = () => (
 
     </form>
   </div>
-);
+);}
 
 export default Create;
